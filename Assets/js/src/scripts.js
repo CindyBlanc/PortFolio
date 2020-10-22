28ce8280f1a48335702171056eecf51da319dc38
Cindy Blanc <ciblanc.formation@gmail.com>
2020-10-22 12:03:23 +0200

    fin de matinée du 22/10

--- Assets/js/src/scripts.js
+++ Assets/js/src/scripts.js
@@ -1,84 +1,62 @@
 // Fonction pour le message d'accueil
 // https://webdevpro.net/js-effet-apparition-lettres/
-const htmlP = document.getElementById("event_accueil");
-const txt = htmlP.dataset.label;
-let i 	= 0 ;
 
-function showLetters()
-{
-  let timeOut ;
-  if(i < txt.length)
-	{
-	  htmlP.innerHTML += `<span>${txt[i]}</span>` ;
-	  timeOut = setTimeout(showLetters,50)
-	  i++
-	}
-	else
-	{
-	  clearTimeout(timeOut);
-	  console.log("end")
-	}
-}
-showLetters();
+$(function() { 
+  const htmlP = document.getElementById("event_accueil");
+  const txt = htmlP.dataset.label;
+  let i 	= 0 ;
 
-
-// Fonction pour le scrolling de la navbar 
-
-
-// $(function() {
-//     $(".loader").fadeOut("1000"); 
-// });
-
-let navbar = document.getElementById("navBar");
-let sticky = window.scrollY;
-
-console.log(sticky);
-
-$(function() {
-    if(window.scrollY >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
+  function showLetters()
+  {
+    let timeOut ;
+    if(i < txt.length)
+    {
+      htmlP.innerHTML += `<span>${txt[i]}</span>` ;
+      timeOut = setTimeout(showLetters,50)
+      i++
     }
-    
+    else
+    {
+      clearTimeout(timeOut);
+      console.log("end")
+    }
+  };
+
+
+  // Fonction pour le scrolling de la navbar 
+
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
+
+  // Get the navbar
+  var navbar = document.getElementById("menu_navBar");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+
+
+
+  // Fonction pour afficher dans un alert dès qu'on click sur Envoyer
+
+    $(".inputBtn").click(function () { 
+      alert("Votre message a bien été envoyé !");
+      
+    }); 
+ 
+   
+  // Fonction changement de couleur à un changement de scroll 
+  
+
+
 });
-
-
-
-
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-
-
-
-
-// <div id="navbar">
-//   <a class="active" href="javascript:void(0)">Home</a>
-//   <a href="javascript:void(0)">News</a>
-//   <a href="javascript:void(0)">Contact</a>
-// </div>
-
-
-
-// Fonction pour afficher dans un alert ce qui est écrit dans les input de contact 
-
-// https://apcpedagogie.com/recuperer-la-valeur-dun-input-en-javascript/ 
-
-
-function getValue() {
-    var varName = document.getElementsByClassName("varName").value;
-    var varEmail = document.getElementsByClassName("varEmail").value;
-    var varTel = document.getElementsByClassName("varTel").value;
-    var varCom = document.getElementsByClassName("varCom").value;
-
-    alert("Bienvenue " + varName + ". Votre email est " + varEmail + " et votre numéro de téléphone est " + varTel + ". Voici votre commentaire : " + varCom);
-}
